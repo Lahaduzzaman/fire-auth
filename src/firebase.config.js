1684ee49352a4d438e91e2c457f699dc35078135
Lahaduzzaman <criclahad.2@gmail.com>
2021-03-15 15:21:47 +0600

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAqIuCMZecw_oYe17jgl_Z6gYKIm30dYPI",
+    authDomain: "ema-john-simple-dcf0c.firebaseapp.com",
+    projectId: "ema-john-simple-dcf0c",
+    storageBucket: "ema-john-simple-dcf0c.appspot.com",
+    messagingSenderId: "713385366206",
+    appId: "1:713385366206:web:f62251cccb3b472b9784ef"
+  };
+
+  export default firebaseConfig;
